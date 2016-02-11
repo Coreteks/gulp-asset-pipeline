80943e3f31aaacdcc121d060f3a5c4693a895446
Tom Rochette <roctom@gmail.com>
2016-02-11 16:17:40 -0500

    Update watch task to use tasks name function instead of hardcoded string. Update watch on ts scripts so that if the there is no lint task for the given bundle, we do not attempt to run a missing task.

--- tasks/watch.js
+++ tasks/watch.js
@@ -2,15 +2,26 @@
 let gulp = require('gulp');
 let config = require('../config');
 let _ = require('lodash');
+let runSequence = require('run-sequence');
+let jsTaskName = require('./js').taskName;
+let tsTaskName = require('./ts').taskName;
+let tsLintTaskName = require('./ts-lint').taskName;
+let sassTaskName = require('./sass').taskName;
 
 gulp.task('watch', function() {
 	_.forEach(config.js, function(bundle) {
-		gulp.watch(bundle.src, ['js-' + bundle.name]);
+		gulp.watch(bundle.src, [jsTaskName(bundle)]);
 	});
 	_.forEach(config.ts, function(bundle) {
-		gulp.watch(bundle.src, ['ts-lint-' + bundle.name, 'ts-' + bundle.name]);
+		if (gulp.hasTask(tsLintTaskName(bundle))) {
+			gulp.watch(bundle.src, function() {
+				return runSequence(tsLintTaskName(bundle), tsTaskName(bundle));
+			});
+		} else {
+			gulp.watch(bundle.src, [tsTaskName(bundle)]);
+		}
 	});
 	_.forEach(config.sass, function(bundle) {
-		gulp.watch(bundle.src, ['sass']);
+		gulp.watch(bundle.src, [sassTaskName(bundle)]);
 	});
 });
