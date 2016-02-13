d5a47d578704e9b274cab7b5377923a4c5dcff3f
Tom Rochette <roctom@gmail.com>
2016-02-13 11:28:51 -0500

    If a tsconfig.json is available, use it when running the ts-* tasks.

--- tasks/ts.js
+++ tasks/ts.js
@@ -3,6 +3,7 @@ let gulp = require('gulp');
 let config = require('../config');
 let $ = require('gulp-load-plugins')();
 let _ = require('lodash');
+let fs = require('fs');
 let path = require('path');
 
 let prefix = 'ts';
@@ -17,13 +18,20 @@ gulp.task(prefix, _.map(bundles, function(bundle) {
 	return taskName(bundle);
 }));
 
+let tsConfigFilename = 'tsconfig.json';
+let tsProject;
+// If a tsconfig.json file exist, use it
+if (fs.existsSync(tsConfigFilename)) {
+	tsProject = $.typescript.createProject(tsConfigFilename);
+}
+
 _.forEach(bundles, function(bundle, key) {
 	gulp.task(taskName(bundle), function() {
 		return gulp.src(bundle.src)
 			.pipe($.newer(path.resolve(bundle.dest, bundle.name)))
 			.pipe($.if(config.debug, $.debug()))
 			.pipe($.if(bundle.sourcemaps, $.sourcemaps.init()))
-			.pipe($.typescript())
+			.pipe($.typescript(tsProject))
 			.pipe($.concat(bundle.name))
 			.pipe($.if(config.production, $.uglify()))
 			.pipe($.if(bundle.sourcemaps, $.sourcemaps.write('.')))
